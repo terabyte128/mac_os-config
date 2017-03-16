43e3b220540384f756a6d26b36c89208fbaae540
Sam Wolfson <wolfson@cs.uw.edu>
2017-03-16 14:06:05 -0700

    Customize for me

--- lib/settings.sh
+++ lib/settings.sh
@@ -21,6 +21,10 @@ export REPO_SUBLIME_TEXT_SETUP=v6.0.0
 export REPO_DOTFILES=v24.2.0
 
 # Applications
+
+export ATOM_APP_NAME="Atom.app"
+export ATOM_APP_URL="https://atom.io/download/mac"
+
 export ALFRED_APP_NAME="Alfred 3.app"
 export ALFRED_VOLUME_NAME="Alfred"
 export ALFRED_APP_URL="https://cachefly.alfredapp.com/Alfred_3.2.1_768.dmg"
@@ -40,17 +44,6 @@ export CHEATSHEET_APP_URL="https://www.cheatsheetapp.com/CheatSheet/download.php
 export CHROME_APP_NAME="Google Chrome.app"
 export CHROME_VOLUME_NAME="Google Chrome"
 export CHROME_APP_URL="https://dl.google.com/chrome/mac/stable/GGRM/googlechrome.dmg"
-
-export CLOAK_APP_NAME=Cloak.app
-export CLOAK_VOLUME_NAME="Cloak"
-export CLOAK_APP_URL="https://static.getcloak.com/downloads/osx/updates/Release/Cloak-2.1.2.dmg"
-
-export DASH_APP_NAME=Dash.app
-export DASH_APP_URL="https://sanfrancisco.kapeli.com/downloads/v3/Dash.zip"
-
-export DOXIE_APP_NAME=Doxie.app
-export DOXIE_VOLUME_NAME="Doxie"
-export DOXIE_APP_URL="http://www.getdoxie.com/resources/files/download_current_mac.php"
 
 export DROPBOX_APP_NAME=Dropbox.app
 export DROPBOX_VOLUME_NAME="Dropbox Installer"
